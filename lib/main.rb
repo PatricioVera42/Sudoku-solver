9bba7eef3ef8df91a6c3c0ee9c095da56e698a70
patovera <verapatricio9@gmail.com>
2024-06-14 00:44:48 -0300

    implementado seleccion de dificultad y elegir un tablero al azar

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,22 @@
+puts "Que Dificultad? \n (f,m o d)"
+dificultad = gets.chomp
+while (dificultad != "f" && dificultad != "m" && dificultad != "d")
+  puts "Caracter invalido, elegir entre f (facil), m (medio) o d (dificil) \n"
+  dificultad = gets.chomp
+end
+case dificultad
+when  "f"
+  coleccion_tableros =  File.readlines("tableros/facil.txt")
+when "m"
+  coleccion_tableros =  File.readlines("tableros/medio.txt")  
+when "d"
+  coleccion_tableros =  File.readlines("tableros/dificil.txt") 
+end
 
-
-tablero_faciles = File.readlines('tableros/facil.txt');
 arreglo = Array.new
 
-tablero_faciles.each do |tablero|
+coleccion_tableros.each do |tablero|
   arreglo.push(tablero.strip)
 end
 
-putsclear
- arreglo[0..5]
+puts arreglo[rand(0..arreglo.length)]
